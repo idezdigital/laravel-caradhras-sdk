6bd0eb094865808d3a1db5b2fb2d7133a69d49ab
Iago Queiroz <iago.queiroz@hotmail.com>
2022-07-21 18:20:02 -0300

    refactor(alias): update exceptions for alias bank creation

---
+++ tests/Unit/Clients/CaradhrasAliasClientTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Idez\Caradhras\Tests\Unit\Clients;
+
+use Idez\Caradhras\Tests\TestCase;
+use Illuminate\Http\Client\Request;
+use Illuminate\Support\Facades\Http;
+use Idez\Caradhras\Enums\AliasBankProvider;
+use Illuminate\Foundation\Testing\WithFaker;
+use Idez\Caradhras\Clients\CaradhrasAliasClient;
+use Idez\Caradhras\Exceptions\CaradhrasException;
+
+class CaradhrasAliasClientTest extends TestCase
+{
+    use WithFaker;
+
+    private CaradhrasAliasClient $aliasClient;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->aliasClient = app(CaradhrasAliasClient::class);
+    }
+
+    public function testIfThrowsCorrectExceptionWhenAccountHasPendingDocuments(): void
+    {
+        $accountId = $this->faker->randomNumber(5);
+
+        $expectedRequestUrl = $this->aliasClient->getApiBaseUrl() . "/v1/accounts";
+
+        $fakeResponse = [
+            "message" => "Transaction not allowed due to lack of regulatory informations or documents.",
+        ];
+
+        Http::fake([
+            $expectedRequestUrl => Http::response($fakeResponse, 409),
+        ]);
+
+        $this->expectException(CaradhrasException::class);
+        $this->expectExceptionMessage("A conta possui informações ou documentos pendentes.");
+
+        $this->aliasClient->findOrCreate($accountId, AliasBankProvider::Votorantim);
+
+        Http::assertSent(
+            fn (Request $request) => $request->url() === $expectedRequestUrl
+        );
+    }
+}
