cf756f8fc8ba8eb04f6807eda5b2f7ea3129f9e5
Pedro Oliveira <pedro@idez.com.brl>
2022-09-05 18:15:15 -0300

    updates caradhras service provider

---
+++ src/Clients/CaradhrasLimitsClient.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Idez\Caradhras\Clients;
+
+use Idez\Caradhras\Clients\BaseApiClient;
+
+class CaradhrasLimitsClient extends BaseApiClient
+{
+    public const API_PREFIX = 'limits';
+
+    public function createLimit(string $accountId, int $serviceGroup, int $limitType, float $amount)
+    {
+        return $this->apiClient()->post("/limits/v2/accounts/{$accountId}/services-groups/{$serviceGroup}", [
+            'limitType' => (string) $limitType,
+            'requestLimit' => $amount
+        ])->object();
+    }
+
+    public function createBatchLimit(string $accountId, int $serviceGroup, array $limits)
+    {
+        return $this->apiClient()->post("/limits/v2/accounts/{$accountId}/batches", [
+            'idServicesGroup' => $serviceGroup,
+            'limits' => $limits
+        ])->object();
+    }
+
+    public function getLimitById(string $requestId)
+    {
+        return $this->apiClient()->get('/limits/v2/requests', [
+            'idRequest' => $requestId
+        ])->object();
+    }
+}
