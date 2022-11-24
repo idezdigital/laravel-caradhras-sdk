a6be5b1c8b750d373993c3ebffd68458bcbc44ef
Davi Kaipper <davi0kaipper@gmail.com>
2022-11-24 15:06:27 -0300

    feat: migrate getTransfer method

---
+++ src/Data/P2PTransferPayload.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Idez\Caradhras\Data;
+
+use App\Enums\Caradhras\Transfers\P2PStatus;
+use Carbon\Carbon;
+
+class P2PTransferPayload
+{
+    /** @var string */
+    public $transactionCode;
+
+    /** @var int */
+    public $originalAccount;
+
+    /** @var int */
+    public $destinationAccount;
+
+    /** @var string */
+    public $description;
+
+    /** @var int */
+    public $idAdjustment;
+
+    /** @var int */
+    public $idIssuer;
+
+    /** @var int */
+    public $idAdjustmentDestination;
+
+    /** @var mixed */
+    public $amount;
+    public $transactionDate;
+
+    /** @var P2PStatus */
+    public $status;
+
+    private $raw;
+
+    public function __construct(array $payload)
+    {
+        $this->transactionCode = $payload['transactionCode'];
+        $this->originalAccount = $payload['originalAccount'];
+        $this->destinationAccount = $payload['destinationAccount'];
+        $this->description = $payload['description'];
+        $this->idAdjustment = $payload['idAdjustment'];
+        $this->idIssuer = $payload['idIssuer'];
+        $this->idAdjustmentDestination = $payload['idAdjustmentDestination'];
+        $this->amount = $payload['amount'];
+        $this->transactionDate = Carbon::parse($payload['transactionDate']);
+        $this->status = P2PStatus::tryFrom($payload['status']);
+        $this->raw = $payload;
+    }
+
+    public function toArray(): array
+    {
+        return $this->raw;
+    }
+}
