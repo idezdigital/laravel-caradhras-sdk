4ad6b236213303225f5225fec19a21d663f2763b
Pedro Santiago <contato@pedrosantiago.com.br>
2023-03-06 11:58:51 -0300

    Update caradhras.php

--- config/caradhras.php
+++ config/caradhras.php
@@ -1,13 +1,13 @@
 <?php
 
 return [
-    'enable_sentry_middleware' => env('CARADHRAS_ENABLE_SENTRY_MIDDLEWARE', true),
-    'services' => [
-        'endpoint' => env('CARADHRAS_ENDPOINT', 'hml.caradhras.io'),
-    ],
-    'app' => [
+     'endpoint' => env('CARADHRAS_ENDPOINT', 'hml.caradhras.io'),
+     'client' => env('CARADHRAS_CLIENT'),
+     'secret' => env('CARADHRAS_SECRET'),
+     'enable_sentry_middleware' => env('CARADHRAS_ENABLE_SENTRY_MIDDLEWARE', true),
+     'app' => [
         'product_id' => (int) env('PRODUCT_ID', 1),
         'business_source_id' => (int) env('BUSINESS_SOURCE_ID', 1),
         'plastic_id' => (int) env('PLASTIC_ID', 2),
-    ],
+     ],
 ];
