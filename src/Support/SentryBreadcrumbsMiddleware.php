65f58428117214803aa6ae0e5bbd2ae1bb6efb38
Arthur Tavares <arthurabreu00@gmail.com>
2023-04-12 11:16:36 -0300

    add sentry scop tag

--- src/Support/SentryBreadcrumbsMiddleware.php
+++ src/Support/SentryBreadcrumbsMiddleware.php
@@ -31,6 +31,7 @@ class SentryBreadcrumbsMiddleware
                 return $handler($request, $options)->then(function (ResponseInterface $response) use ($sentry) {
                     $level = Breadcrumb::LEVEL_DEBUG;
                     if ($response->getStatusCode() > 400) {
+                        $sentry->configureScope(fn($scope) => $scope->setTag('service', $prefix));
                         $level = Breadcrumb::LEVEL_ERROR;
                     }
 
