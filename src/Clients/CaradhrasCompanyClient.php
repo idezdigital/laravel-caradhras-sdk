0093b3884bcf4b543bff433bcfaa35c46d6c4ead
Felipe Silva <felipe.dacs@gmail.com>
2023-05-15 20:38:09 -0300

    feat: adds pending document method

--- src/Clients/CaradhrasCompanyClient.php
+++ src/Clients/CaradhrasCompanyClient.php
@@ -215,4 +215,11 @@ class CaradhrasCompanyClient extends BaseApiClient
 
         return new CompanyDocument($response->object()->result);
     }
+
+    public function getPendingAccountDocuments(string $registrationId): object
+    {
+        return $this->apiClient()
+            ->get("/v1/registrations/{$registrationId}/documents/status")
+            ->object();
+    }
 }
