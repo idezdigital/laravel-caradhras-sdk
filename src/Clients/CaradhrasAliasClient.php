5d4caea3901f51c7992018dd0024dbceaae6af80
Pedro Santiago <pedro@idez.com.br>
2023-09-13 19:58:11 -0300

    updates

--- src/Clients/CaradhrasAliasClient.php
+++ src/Clients/CaradhrasAliasClient.php
@@ -2,13 +2,17 @@
 
 namespace Idez\Caradhras\Clients;
 
+use Exception;
 use Idez\Caradhras\Enums\AliasBankProvider;
 use Idez\Caradhras\Exceptions\CaradhrasException;
+use Illuminate\Http\Client\PendingRequest;
 use Illuminate\Http\Client\Response;
 
 class CaradhrasAliasClient extends BaseApiClient
 {
     public const API_PREFIX = 'aliasbank';
+
+    private const ERROR_LACK_REGULATORY = "Transaction not allowed due to lack of regulatory informations or documents.";
 
     /**
      * Create an alias.
@@ -45,35 +49,31 @@ class CaradhrasAliasClient extends BaseApiClient
 
         $response = $this
             ->apiClient(false)
+            ->retry(5, 2000, fn (Exception $exception, PendingRequest $request) => $exception->getCode() >= 500, false)
             ->post('/v1/accounts', [
                 'idAccount' => $accountId,
                 'bankNumber' => $bankProvider->value,
             ]);
 
-        if (! in_array($response->status(), [200, 201, 202, 409])) {
+        if ($response->failed()) {
             throw match ($response->status()) {
                 400 => new CaradhrasException('Conta inválida.', $response->status()),
                 404 => new CaradhrasException('Conta não encontrada.', $response->status()),
+                409 => new CaradhrasException($response->json('message') === self::ERROR_LACK_REGULATORY
+                    ? "A conta possui informações ou documentos pendentes."
+                    : "Erro Desconhecido", $response->status()),
                 default => new CaradhrasException('Tente novamente mais tarde.', $response->status()),
             };
         }
 
-        $responseObject = $response->object();
-
-        if (
-            $response->status() === 409 &&
-            $responseObject?->message === "Transaction not allowed due to lack of regulatory informations or documents."
-        ) {
-            throw new CaradhrasException("A conta possui informações ou documentos pendentes.", $response->status());
-        }
-
-        return $responseObject->data;
+        return $response->object()?->data;
     }
 
     public function find(int $accountId, AliasBankProvider $bankProvider = AliasBankProvider::Votorantim): array
     {
         $response = $this
             ->apiClient(false)
+            ->retry(3, 2000)
             ->get('/v1/accounts', [
                 'idAccount' => $accountId,
                 'bankNumber' => $bankProvider->value,
