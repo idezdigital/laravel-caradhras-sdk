3b9c32f5fe23248e50a4b6067d36bed90049f9c6
Rafael Laurindo <rafaellaurindomf@icloud.com>
2024-03-07 18:39:18 -0300

    feat: add label to alias bank provider enum

--- src/Enums/AliasBankProvider.php
+++ src/Enums/AliasBankProvider.php
@@ -8,4 +8,16 @@ enum AliasBankProvider: string
     case BTG = '208';
     case Itau = '341';
     case Dock = '301';
+
+    public function label(): string
+    {
+        $bankName = match ($this) {
+            self::Votorantim => 'Votorantim',
+            self::BTG => 'BTG Pactual',
+            self::Itau => 'Itaú',
+            self::Dock => 'Dock',
+        };
+
+        return "$bankName ($this->value)";
+    }
 }
