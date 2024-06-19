650cc42ace0bd32ef5432634f643139ba9b20cea
Pedro Lucas <pedro.lucaswpl@gmail.com>
2024-06-19 13:51:59 -0300

    ajuste

--- src/Clients/CaradhrasCardClient.php
+++ src/Clients/CaradhrasCardClient.php
@@ -409,7 +409,7 @@ class CaradhrasCardClient extends BaseApiClient
 
         return array_map(
             fn ($cardMccGroupControl) => new CardMccGroupControl($cardMccGroupControl),
-            (array) $response->object()
+            $response->json()
         );
     }
 
